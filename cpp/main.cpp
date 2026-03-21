52963de102348005251caca0031ecab6d1dc6503
Dylan Tran <dylanbt928@gmail.com>
2026-03-20 17:06:37 -0700

    lights out in cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,64 +5,114 @@ using namespace pimoroni;
 
 PicoRGBKeypad keypad;
 
-struct Color
+uint16_t maps[5]{ 0xbeb9, 0xe661, 0xb62c, 0x8c1f, 0xa26d };
+uint8_t level{ 0 };
+
+void invert_lights(int key)
 {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-};
+    maps[level] ^= (1 << key);
 
-constexpr uint8_t num_colors{ 6 };
+    // top
+    if (key > 3)
+    {
+        maps[level] ^= 1 << (key - 4);
+    }
 
-Color colors[num_colors]{
-    { 0x00, 0x20, 0x00 },  // green
-    { 0x20, 0x20, 0x00 },  // yellow
-    { 0x20, 0x00, 0x00 },  // red
-    { 0x20, 0x00, 0x20 },  // pink
-    { 0x00, 0x00, 0x20 },  // blue
-    { 0x00, 0x20, 0x20 }   // teal
-};
+    // bottom
+    if (key < 12)
+    {
+        maps[level] ^= 1 << (key + 4);
+    }
+
+    // left
+    if (key % 4 != 0)
+    {
+        maps[level] ^= 1 << (key - 1);
+    }
+
+    // right
+    if (key % 4 != 3)
+    {
+        maps[level] ^= 1 << (key + 1);
+    }
+}
+
+void win_lights()
+{
+    for (size_t i{ 0 }; i < 3; ++i)
+    {
+        for (int key{ 0 }; key < PicoRGBKeypad::NUM_PADS; ++key)
+        {
+            keypad.illuminate(key, 0x00, 0x55, 0x00);
+        }
+
+        keypad.update();
+        sleep_ms(200);
+
+        for (int key{ 0 }; key < PicoRGBKeypad::NUM_PADS; ++key)
+        {
+            keypad.illuminate(key, 0x00, 0x00, 0x00);
+        }
+
+        keypad.update();
+        sleep_ms(200);
+    }
+}
 
 int main()
 {
     keypad.init();
     keypad.set_brightness(1.0f);
 
-    uint16_t lit{ 0 };
-    uint8_t color_index{ 0 };
-    Color curr_color{ colors[color_index] };
+    uint16_t last_button_states{ 0 };
 
     while (true)
     {
-        lit |= keypad.get_button_states();
+        uint16_t curr_button_states{ keypad.get_button_states() };
+        uint16_t new_button_states{ curr_button_states & ~last_button_states };
 
-        for (size_t i{ 0 }; i < PicoRGBKeypad::NUM_PADS; ++i)
+        last_button_states = curr_button_states;
+
+        if (new_button_states > 0)
         {
-            if (lit & (0x1 << i))
+            for (int key{ 0 }; key < PicoRGBKeypad::NUM_PADS; ++key)
             {
-                keypad.illuminate(i, curr_color.r, curr_color.g, curr_color.b);
+                // button pressed
+                if ((new_button_states >> key) & 0x01)
+                {
+                    invert_lights(key);
+
+                    // completed level
+                    if (maps[level] == 0xffff)
+                    {
+                        win_lights();
+                        ++level;
+
+                        // completed all levels
+                        if (level == sizeof(maps) / sizeof(maps[0]))
+                        {
+                            return 0;
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        for (int key{ 0 }; key < PicoRGBKeypad::NUM_PADS; ++key)
+        {
+            if ((maps[level] >> key) & 0x01)
+            {
+                keypad.illuminate(key, 0x55, 0x22, 0x22);
             }
             else
             {
-                keypad.illuminate(i, 0x05, 0x05, 0x05);
+                keypad.illuminate(key, 0x00, 0x00, 0x00);
             }
         }
 
         keypad.update();
         sleep_ms(100);
-
-        if (lit == 0xffff)
-        {
-            sleep_ms(500);
-            lit = 0;
-            ++color_index;
-
-            if (color_index == num_colors)
-            {
-                color_index = 0;
-            }
-
-            curr_color = colors[color_index];
-        }
     }
 }
